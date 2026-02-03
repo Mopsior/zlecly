5c188eee2fa9edf1a9563ff59b6923471896920d
Mopsior <bartek.szg@gmail.com>
2026-02-03 13:42:10 +0100

    chore: add mobile settings button
    - added UserButton back. To be observed later
    - fixed scrollbar on auth pages

--- src/features/clerk/wrapper.tsx
+++ src/features/clerk/wrapper.tsx
@@ -1,6 +1,6 @@
 export const Wrapper = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className='flex h-dvh w-full justify-center not-md:items-center md:mt-20'>
+        <div className='flex h-dvh w-full justify-center not-md:items-center md:pt-20'>
             {children}
         </div>
     )
--- src/features/routing/tabs.tsx
+++ src/features/routing/tabs.tsx
@@ -1,8 +1,9 @@
-import { ChartNoAxesColumn, Share2 } from 'lucide-react'
+import { ChartNoAxesColumn, Settings2, Share2 } from 'lucide-react'
 import { useTranslation } from 'react-i18next'
-import { useLocation, useNavigate } from '@tanstack/react-router'
+import { Link, useLocation, useNavigate } from '@tanstack/react-router'
 import { Return } from '../return'
-// import { UserButton } from '../clerk/user-button'
+import { UserButton } from '../clerk/user-button'
+import { Button } from '../ui/button'
 import { selectedRouteTab } from './types'
 import { Tabs as ShadTabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
@@ -34,8 +35,13 @@ export const Tabs = () => {
                     </TabsTrigger>
                 </TabsList>
             </ShadTabs>
+            <Link to='/app/settings' className='md:hidden'>
+                <Button variant='secondary'>
+                    <Settings2 />
+                </Button>
+            </Link>
             <div className='fixed top-13 right-8 -translate-y-1/2 transform not-md:hidden'>
-                {/* <UserButton /> */}
+                <UserButton />
             </div>
         </div>
     )
